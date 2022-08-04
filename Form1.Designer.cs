826efa5adc9cb6f6a10cada78b7a54eff45b2f62
Satoshi Kawano <kawanoengsol@eng-sol.co.jp>
2022-08-04 14:15:44 +0900

    ブランチテスト

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace WindowsFormsApp1
 			this.button1 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -59,11 +60,19 @@ namespace WindowsFormsApp1
 			this.button2.Text = "button2";
 			this.button2.UseVisualStyleBackColor = true;
 			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(138, 84);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(366, 25);
+			this.textBox2.TabIndex = 3;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button1);
@@ -79,6 +88,7 @@ namespace WindowsFormsApp1
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
 	}
 }
 
